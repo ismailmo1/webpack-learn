03cdc3af92b97a53a30bbc1a2c94ab1a2eacbc72
Ismail Mohammed <ismailmo4@gmail.com>
2022-07-02 22:32:11 +0100

    Add asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import _ from 'lodash';
+import CSVData from './data.csv';
+import JSONData from './data.json';
+import XMLData from './data.xml';
+import Icon from './icon.png';
+import './style.css';
 
 function component() {
     const element = document.createElement('div');
 
     // lodash imported above
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    // add image
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
+    console.log(CSVData, XMLData, JSONData);
 
     return element;
 }
